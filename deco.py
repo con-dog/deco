9ff2f1d03c150cc734eaba5e1c833f9b10e70733
Connor <connordanielfrank@gmail.com>
2022-12-18 16:38:26 +1300

    dev: add speedtest, memoization

--- deco.py
+++ deco.py
@@ -9,3 +9,64 @@ modifying how the function is called.
 
 Examples:
 """
+from typing import Callable, Dict, TypeVar
+
+import timeit
+
+
+T = TypeVar('T')
+
+def speedtest(func: Callable[..., T]) -> Callable[..., T]:
+    """Decorator that measures the execution time of the decorated function.
+
+    Args:
+        func (callable): The function to be decorated.
+
+    Returns:
+        callable: The decorated function.
+
+    Examples:
+        @speedtest
+        def foo(x, y):
+            return x + y
+
+        foo(3, 4)
+        # Output: foo took 0.000001 seconds
+    """
+    def wrapper(*args, **kwargs):
+        start = timeit.default_timer()
+        result = func(*args, **kwargs)
+        end = timeit.default_timer()
+        print(f'[*] {func.__name__} took {end - start:.6f} seconds')
+        return result
+    return wrapper
+
+def memoize(func: Callable[..., T]) -> Callable[..., T]:
+    """A decorator that adds memoization behavior to a function.
+
+    Args:
+        func: The function to be decorated.
+
+    Returns:
+        A new function with memoization behavior.
+    """
+    cache: Dict[tuple, T] = {}
+
+    def wrapper(*args: tuple) -> T:
+        if args in cache:
+            return cache[args]
+        result = func(*args)
+        cache[args] = result
+        return result
+
+    return wrapper
+
+
+@memoize
+def loopy():
+    total = 0
+    for i in range(100000):
+        total += i
+
+
+loopy()
