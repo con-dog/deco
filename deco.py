b87a1e8250d6030ca743be771f06a537daefb9a9
Connor <connordanielfrank@gmail.com>
2022-12-18 16:01:18 +1300

    build: Initial commit

---
+++ deco.py
@@ -0,0 +1,11 @@
+"""
+deco.py
+
+A library of useful Python function decorators.
+
+Decorators allow you to modify the behavior of a function without changing its
+code. This can be useful for adding functionality, enforcing constraints, or
+modifying how the function is called.
+
+Examples:
+"""
